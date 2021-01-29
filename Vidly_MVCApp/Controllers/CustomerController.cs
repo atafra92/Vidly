15d8a9568c908f88d058f46f9e891c96a9529b87
Antonio Tafra <antonio.tafra@gmail.com>
2021-01-29 13:54:05 +0100

    Exercise 1 - Fundementals
    Added a Movie model and controller, added a Customer model and controller

---
+++ Vidly_MVCApp/Controllers/CustomerController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly_MVCApp.Models;
+
+namespace Vidly_MVCApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private List<Customer> ListCustomers()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "Antonio"},
+                new Customer { Id = 2, Name = "Ivana" }
+            };
+
+            return customers;
+        }
+
+        public IActionResult GetCustomers()
+        {
+            var customers = ListCustomers();
+            
+            return View(customers);
+        }
+
+        
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = ListCustomers().FirstOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+     
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Vidly_MVCApp/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly_MVCApp.Models;
+
+namespace Vidly_MVCApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        private List<Movie> ListMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Title = "Star Wars VI - Return of the Jedi" },
+                new Movie { Id = 2, Title = "Star Wars V - The Empire Strikes Back" }
+            };
+
+            return movies;
+        }
+        //GET: Movies
+        public IActionResult GetMovies()
+        {
+            var movies = ListMovies();
+
+            return View(movies);
+        }
+
+       public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = ListMovies().FirstOrDefault(m => m.Id == id);
+
+            return View(movie);
+        }
+    }
+}
